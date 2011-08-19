de33fe1ab157d7c2bc2a2525d6df3db2e2fa1ae1
Sylvain Viollon <thefunny@gmail.com>
2011-08-19 18:41:33 +0200

    Add IBatching to __init__ to simplify imports.

--- src/zeam/utils/batch/__init__.py
+++ src/zeam/utils/batch/__init__.py
@@ -1,10 +1,10 @@
 
 from zeam.utils.batch.batch import Batch
 from zeam.utils.batch.date.batch import DateBatch, BATCH_DAY, BATCH_MONTH
-
+from zeam.utils.batch.interfaces import IBatching
 
 # BBB
 batch = Batch
 
 
-__all__ = ['Batch', 'DateBatch', 'BATCH_DAY', 'BATCH_MONTH']
+__all__ = ['Batch', 'DateBatch', 'BATCH_DAY', 'BATCH_MONTH', 'IBatching']
